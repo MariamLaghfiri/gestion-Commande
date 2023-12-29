91199c5ed670870ddf8f96545118eb7bfd15f984
Mariam <laghfirimariam@gmail.com>
2023-12-29 15:44:06 +0100

    commande

--- src/main/java/com/orderCraftSpringApp/service/CommandeService.java
+++ src/main/java/com/orderCraftSpringApp/service/CommandeService.java
@@ -1,11 +1,13 @@
 package com.orderCraftSpringApp.service;
 
+import com.orderCraftSpringApp.model.Client;
 import com.orderCraftSpringApp.model.Commande;
 import com.orderCraftSpringApp.repository.CommandeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommandeService {
@@ -15,6 +17,15 @@ public class CommandeService {
 
     public  List<Commande> getAllCommandes(){
         return commandeRepository.findAll();
+    }
+    public Optional<Commande> getCommandeById(Long id) {
+        return commandeRepository.findById(id);
+    }
+    public Commande saveCommande(Commande commande){
+        return commandeRepository.save(commande);
+    };
+    public void deleteCommande(Long id) {
+        commandeRepository.deleteById(id);
     }
 
 }
